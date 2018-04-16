b49b9b5489550fbc7e4b766d3bf2191f19c8ed00
liucan <liucan@cd.tuan800.com>
2018-04-16 18:31:59 +0800

    第二次提交

--- src/com/tutorialspoint/HelloWorld.java
+++ src/com/tutorialspoint/HelloWorld.java
@@ -3,10 +3,10 @@ package com.tutorialspoint;
 public class HelloWorld {
    private String message;
    
-   public void setMessage(String message){
+   public void setMessage(String message) {
       this.message  = message;
    }
-   public void getMessage(){
+   public void getMessage() {
       System.out.println("Your Message : " + message);
    }
 }
